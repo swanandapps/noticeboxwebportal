5a2d4262b0e34f5225024667920a8574e7497925
legends287 <32398003+legends287@users.noreply.github.com>
2018-05-01 13:49:57 +0530

    all set

--- app.js
+++ app.js
@@ -1,37 +1,36 @@
-var express = require('express');
-
-
-var mangoosecrate = require('mongoose-crate-s3')
-
-var bodyParser = require('body-parser')
+var express = require('express')
 var mongoose = require('mongoose')
-var app = express();
 
+var app = express();
+var bodyParser = require('body-parser')
+var fs = require('fs');
 mongoose.connect("mongodb://swanandnoticeb:swanand123@ds161459.mlab.com:61459/noticebox");
 
+app.use(bodyParser.json())
 
-
-
-
-
+var multer = require('multer');
+var upload = multer({ dest: './uploads' });
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }))
-
-//initiate GFS
-
 
 //SCHEMA SETUP
 var noticeschema = new mongoose.Schema({
 
 
-        department: String,
+    department: String,
+    name: String,
+    message: String,
+
+    file: ({
         name: String,
-        message: String,
+        data: Buffer,
+        content_type: String,
+        size: String
+    })
 
-    }
 
-)
+})
 
 //naming model on schema
 //var InfoTech = mongoose.model('InfoTech', noticeboxschema)
@@ -42,28 +41,28 @@ var comp = mongoose.model("Computer Science", noticeschema)
 
 
 
-
-app.post("/upload", function(req, res) {
+app.post("/upload", upload.single('file'), function(req, res) {
 
     var department = req.body.department;
-
     var name = req.body.name;
-
     var message = req.body.message;
-    var file = req.body.file
+    var file = req.file
+    file.data = fs.readFileSync(req.file.path)
+    console.log(req.body)
+    console.log(req.file)
 
     var newnotice = {
 
         department: department,
-
         name: name,
         message: message,
 
+        file: file
 
     }
 
-    console.log(department)
-        //Classification of notices as per department selected
+
+    //Classification of notices as per department selected
 
     if (department == 'it') {
 
@@ -108,24 +107,11 @@ app.post("/upload", function(req, res) {
 
         })
 
-        console.log(req.body)
+
     }
 
 
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -137,10 +123,6 @@ app.get("/getdata", function(req, res) {
         if (err) {
             console.log("error")
         } else {
-
-
-
-
             res.render("getdata.ejs", { noticeboxes: allnoticeboards })
 
         }
